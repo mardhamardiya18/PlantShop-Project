80c3d01f574f5abbda24eac30f295350240cca5d
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2022-03-10 14:37:42 +0700

    update product & banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,30 @@ $(document).ready(() => {
         $('.navbar').toggleClass('active', $(this).scrollTop() > $('.navbar').height())
     })
 })
+
+var swiper = new Swiper(".mySwiperProduct", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".bxs-chevron-right-circle",
+        prevEl: ".bxs-chevron-left-circle",
+      },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        350: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        }
+       
+    }
+  });
